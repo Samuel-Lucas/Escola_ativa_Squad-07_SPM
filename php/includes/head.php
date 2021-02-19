b91c0504787c5fef31f25bed190d2ee1d10cc277
Samuel-Lucas <samuel.moreira10@outlook.com>
2021-02-19 13:57:01 -0300

    Corrigindo caminho do style.css

--- php/includes/head.php
+++ php/includes/head.php
@@ -6,7 +6,6 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
         <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="css/style2.css">
         <script src='script/script_materia.js'></script>
         <title>Escola Ativa</title>
     </head>
