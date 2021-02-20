a470eae2a11217ec1e09001f156cc80a6fb19e78
Mario Menezes <mario082@gmail.com>
2021-02-20 17:31:28 -0300

    teste imagem jumbotron

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
     <?php include_once "php/includes/header.php"; ?>
 
           <div class="container mb-5">
-            <a class="link-color" style="text-decoration: none;" href="aplicacaoMetodologia.php"><div class="jumbotron my-4 d-flex align-items-center" style="background: url(img/Logo.jpeg) no-repeat">
-              <h3 class="text-center text-white mt-5">Por que aplicar Metodologias Ativas nas escolas?</h3>
-            </div></a>
+            <a class="link-color" style="text-decoration: none;" href="aplicacaoMetodologia.php">
+              <div class="jumbotron my-4 d-flex align-items-center" style="background: url(img/atividade.jpg) no-repeat">
+                <h3 class="text-center text-white mt-5">Por que aplicar Metodologias Ativas nas escolas?</h3>
+              </div>
+            </a>
 
             <h4 class="text-center text-secondary mb-4">Metodologias Ativas</h4>
 
