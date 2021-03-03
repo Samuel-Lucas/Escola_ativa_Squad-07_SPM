c1f7bd376aec64734809b7ea94192a621d9d894b
IngridPX <72896995+IngridPX@users.noreply.github.com>
2021-03-03 13:08:20 -0300

    Ajuste pagina

--- metodologiasAtivas.php
+++ metodologiasAtivas.php
@@ -2,30 +2,90 @@
 <?php include_once "php/includes/header.php"; ?>
 
         <div class="container mb-4">
-            <div class="jumbotron my-4 d-flex align-items-center" style="background: url(img/mesa1.jpg) no-repeat center center"></div>
+            <div class="jumbotron my-4 d-flex align-items-center" style="background: url(img/atividade.jpg) no-repeat"></div>
             <div id="container-historia">
-                <h3 class="text-center text-secondary mb-4"> Metodologias Ativas </h3><br/>
-                <h4> Comunidade escola ativa: Juntos somos mais Fortes!</h4><br/>
-                Materiais para alavancar o aprendizado através de metodologias ativas dentro e fora da sala de aula.<br/><br/>
-                Compartilhamento de boas práticas entre escolas e fortalecimento da rede de ensino.</p><br/>
-                <div class="row d-flex flex-wrap">
-                    <div class="card-body mx-auto ensino">
-                        <a href="ensinoFundamental.php"><h6 class="card-title hover"><strong>Metodologias Ativas para Ensino Fundamental</strong></h6>
-                        <img src="img/ensinoFundamental.png" alt="Metodologias Ativas para Ensino Fundamental"></a>
+                <h3 class="text-center text-secondary mb-4 section-title-center"> Metodologias Ativas </h3><br/>
+                <p>A Metodologia Ativa de Aprendizagem pode englobar diferentes práticas dentro e fora da sala de aula. 
+                    Pois possui como principal característica a inserção do aluno como protagonista da própria aprendizagem, 
+                    permitindo-os reconhecer os recursos que melhor funcionam para si, enxergar o professor como um companheiro 
+                    de sua jornada e não uma figura detentora e repassador de conhecimento.</p>
+                <p>Para auxiliar os alunos em sua descoberta dos recursos que melhor funcionam para si. Os professores precisam 
+                    propor uma atividade que tenha um objetivo bem definido, aliado a uma proposta metodológica adequada. Pensando 
+                    nisso, iremos descrever alguns dos tipos de Metodologia Ativa de Aprendizagem:
+                <ul class= "lista">
+                    <li>&numsp; Aprendizagem Baseada Em Projetos (ABP)</li>
+                    <div class="metodologia">
+                        <p>A aprendizagem baseada em projetos (ABP), (do inglês, project based learning (PBL)) – auxilia os alunos na construção 
+                            de soluções colaborativas de desafios, a fim de desenvolver a criação e exploração de soluções, assim como um senso 
+                            crítico e investigativo.
+                        </p>
+                        <p>Na ABP, os alunos devem buscar explorar as possíveis soluções por si mesmo, pois o professor não deve revelar todo o 
+                            conteudo de ensino, para que os alunos possam alcançar os propósitos de aprendizado propostos.
+                        </p>
+                            1.&numsp; O professor apresenta um problema<br />
+                            2.&numsp; Os alunos investigam as causas<br />
+                            3.&numsp; Com base em suas vivências, os alunos elaboram um plano<br />
+                            4.&numsp; Apresentam o plano<br />
+                            5.&numsp; O professor fornece feedbacks dos erros e acertos durante o processo<br />
                     </div>
-                    <div class="card-body mx-auto ensino desativado">
-                        <h6 class="card-title"><strong>Metodologias Ativas para Ensino Médio</strong></h6>
-                        <img src="img/ensinoMedio.png" alt="Card image cap">
+                    <li>&numsp; Aprendizagem Baseada Em Problemas (ABP)</li>
+                    <div class="metodologia">
+                        <p>Na Aprendizagem baseada em problemas, os alunos devem ser capazes de construir um aprendizado conceitual, na qual os alunos 
+                            se reúne em grupos e debatem sobre um problema para encontrar soluções.
+                        </p>
+                        <p>A diferença entre a Aprendizagem Baseada Em Projetos é que a ABP exige que os alunos coloquem a mão na massa, já na 
+                            aprendizagem baseada em problemas, os alunos focam na resolução teórica dos casos.
+                        </p>
+                    </p>
                     </div>
-                    <div class="card-body mx-auto ensino desativado">
-                        <h6 class="card-title"><strong>Metodologias Ativas para EJA</strong></h6><br/>
-                        <img src="img/ensinoEAD.png" alt="Card image cap">
+                    <li>&numsp; Gameficação</li>
+                    <div class="metodologia">
+                        <p>A gamificação é a utilização de mecânicas e dinâmicas de jogos para engajar pessoas, resolver problemas de modo criativo, 
+                            melhorar o aprendizado e comportamentos em ambientes fora do contexto de jogos.
+                        </p>
+                        <p>Cabe ao professor gamificar aspectos normais da sala de aula, de forma, dinâmica, atrativa e inteligente para conquistar 
+                            um maior engajamento dos alunos, mantendo o aprofundamento didático.
+                        </p>
                     </div>
-                    <div class="card-body mx-auto ensino">
-                        <a href="boasPraticas.php"><h6 class="card-title"><strong>Boas práticas Gerais</strong></h6><br/>
-                        <img src="img/boasPraticas.png" alt="Card image cap"></a>
+                    <li>&numsp; Sala De Aula Invertida</li>
+                    <div class="metodologia">
+                        <p>Se no modelo tradicional o professor em uma aula expositiva explica a matéria no quadro para que depois os alunos 
+                            façam, sozinhos, a lição de casa.
+                        </p>
+                        <p>A sala de aula invertida (do inglês, flipped classroom) significa a inversão desta lógica:<br />
+                            Primeiro o aluno faz a internalização dos conceitos essenciais antes de aula e depois, junto à turma, discute os 
+                            conhecimentos adquiridos e tira possíveis dúvidas de conteúdo com a ajuda e orientação do professor.
+                        </p>
+                        <div class="row d-flex flex-wrap">
+                            <div class="card-body mx-auto" style="width: 18rem;">
+                                <p>1. Baseado no tema proposto os alunos fazem as pesquisas</p>
+                                <img src="img/pesquisa_alunos.png" alt="Alunos realizando pesquisa">
+                            </div>
+                            <div class="card-body mx-auto" style="width: 18rem;">
+                                <p>2. Discutir em grupo sobre as informações encontradas</p>
+                                <img src="img/discursao_grupo.png" alt="Discursao em grupo">
+                            </div>
+                            <div class="card-body mx-auto" style="width: 18rem;">
+                                <p>3. Desenvolvem apresentações sobre as discussões</p>
+                                <img src="img/apresentacao.png" alt="Apresentaçao em sala">
+                            </div>
+                        </div>
                     </div>
-                </div>
+                    <li>&numsp; Aprendizagem Entre Pares</li>
+                    <div class="metodologia">
+                        <p>A aprendizagem entre pares e times (do inglês, Peer Instruction (PI) ou team based learning (TBL)), se baseia no 
+                            compartilhamento de ideias, por meio da formação de duplas ou equipes para resolver um projeto ou estudo de caso. 
+                            Na qual, tem se mostrado extremamente benéfica, pois com a ajuda mútua dos alunos é possível aprender e ensinar 
+                            ao mesmo tempo, tornando mais simples a compreensão dos alunos, além de, promover a formação do pensamento crítico 
+                            e a capacidade de respeitarem opiniões divergentes.
+                        </p>
+                    </div>
+                </ul>
+            </p>
+            <p class="fonte">Fonte:guiadocente.com.br/sala-de-aula-invertida-na-pratica<br />
+            Fonte:desafiosdaeducacao.grupoa.com.br/metodologias-ativas<br />
+            Fonte:blog.lyceum.com.br/metodologias-ativas-de-aprendizagem<br />
+            </p>
             </div>
         </div>
 
